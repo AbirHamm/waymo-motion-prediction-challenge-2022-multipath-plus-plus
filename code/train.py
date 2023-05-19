81f4ca1311cc1add2fd293612dc6b486cc2c085c
AbirHamm <55574502+AbirHamm@users.noreply.github.com>
2023-05-19 17:27:41 +0200

    Update train.py

--- code/train.py
+++ code/train.py
@@ -39,9 +39,9 @@ def get_git_revision_short_hash():
 config = get_config(sys.argv[1])
 alias = sys.argv[1].split("/")[-1].split(".")[0]
 #try:
-    models_path = os.path.join("../models", f"{alias}__{get_git_revision_short_hash()}")
-    os.mkdir(tb_path)
-    os.mkdir(models_path)
+models_path = os.path.join("../models", f"{alias}__{get_git_revision_short_hash()}")
+os.mkdir(tb_path)
+os.mkdir(models_path)
 #except:
 #    pass
 last_checkpoint = get_last_file(models_path)
