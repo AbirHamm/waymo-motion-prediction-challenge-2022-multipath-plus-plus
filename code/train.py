50e0d14482ab2879ee10f6c9fb45069e9a12e01e
AbirHamm <55574502+AbirHamm@users.noreply.github.com>
2023-05-22 16:31:42 +0200

    Update train.py

--- code/train.py
+++ code/train.py
@@ -109,7 +109,7 @@ for epoch in tqdm(range(config["train"]["n_epochs"])):
             }
             if config["train"]["scheduler"]:
                 saving_data["scheduler_state_dict"] = scheduler.state_dict()
-            torch.save(saving_data, os.path.join(models_path, f"last.pth"))
+            torch.save(saving_data, os.path.join(models_path, f"/content/drive/MyDrive/output_projet/saving_model/last.pth"))
             print("#################")
             print(f"last.pth")
             print("#################")
